523aaba43fb16bdec029554cdce41e2292ae21a8
Tunisia Artope <artope@yahoo.com>
2022-02-28 11:29:06 -0500

    Initial commit

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,8 +3,9 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('');
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -18,8 +19,12 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter 
+        search={search}
+        onSearchChange={setSearch}
+        onCategoryChange={handleCategoryChange} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
